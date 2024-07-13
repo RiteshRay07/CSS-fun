5a7f3f7650f8db4083cab8264c81f76edbf88737
Soumyacodes2022 <kalpanaganguly.behala94@gmail.com>
2024-07-13 18:37:59 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import Header from './Components/Header'
+import Body from './Components/Body'
+import Footer from './Components/Footer'
+
+const App = () => {
+  return (
+    <>
+      <div className='bg-[#fefce8] min-h-screen px-6 py-4 flex flex-col gap-16 md:px-10 md:py-7 md:justify-between lg:px-18 lg:py-8'>
+        <Header/>
+        <Body/>
+        <Footer/>
+      </div>
+    </>
+
+  )
+}
+
+export default App
