c6f615ee96704b7af99fbc508d6a4f767b3490c8
Soumyacodes2022 <kalpanaganguly.behala94@gmail.com>
2024-07-13 19:00:56 +0530

    main screen resized

--- src/App.jsx
+++ src/App.jsx
@@ -6,7 +6,7 @@ import Footer from './Components/Footer'
 const App = () => {
   return (
     <>
-      <div className='bg-[#fefce8] min-h-screen px-6 py-4 flex flex-col gap-16 md:px-10 md:py-7 md:justify-between lg:px-18 lg:py-8'>
+      <div className='bg-[#fefce8] min-h-screen px-6 py-4 flex flex-col gap-16 md:px-10 md:py-7 md:justify-between lg:px-28 lg:py-8'>
         <Header/>
         <Body/>
         <Footer/>
